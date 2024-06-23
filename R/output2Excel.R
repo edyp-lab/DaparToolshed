13ef1c53089a0078084d276b5aa6f67f4de152c6
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-06-23 19:25:11 +0200

    Update

---
+++ R/output2Excel.R
@@ -0,0 +1,243 @@
+
+
+
+#' @title This function exports a data.frame to a Excel file.
+#'
+#' @param df An data.frame
+#'
+#' @param tags xxx
+#'
+#' @param colors xxx
+#'
+#' @param tabname xxx
+#'
+#' @param filename A character string for the name of the Excel file.
+#'
+#' @return A Excel file (.xlsx)
+#'
+#' @author Samuel Wieczorek
+#'
+#' @export
+#'
+#'
+#' @examples
+#' data(Exp1_R25_pept, package="DAPARdata")
+#' df <- Biobase::exprs(Exp1_R25_pept[seq_len(100)])
+#' tags <- GetMetacell(Exp1_R25_pept[seq_len(100)])
+#' colors <- list(
+#'     "Missing POV" = "lightblue",
+#'     "Missing MEC" = "orange",
+#'     "Quant. by recovery" = "lightgrey",
+#'     "Quant. by direct id" = "white",
+#'     "Combined tags" = "red"
+#' )
+#' write.excel(df, tags, colors, filename = "toto")
+#' 
+NULL
+
+
+#' @export
+#' 
+#' 
+write_Assay_To_Excel <- function(wb, obj, i, n){
+  openxlsx::addWorksheet(wb, names(obj)[i])
+  
+  data <- assay(obj[[i]])
+  openxlsx::writeData(wb, 
+    sheet = n, 
+    cbind(
+      ID = rownames(data),
+      data), 
+    rowNames = FALSE)
+  
+  
+  # Add colors to quantitative table
+  mc <- metacell.def(omXplore::get_type(obj[[i]]))
+  colors <- as.list(stats::setNames(mc$color, mc$node))
+  tags <- cbind(
+    keyId = rep("Quant. by direct id", nrow(obj[[i]])),
+    omXplore::get_metacell(obj[[i]])
+  )
+  
+  
+  unique.tags <- NULL
+  if (!is.null(tags) && !is.null(colors)) {
+    unique.tags <- unique(as.vector(as.matrix(tags)))
+    if (!isTRUE(
+      sum(unique.tags %in% names(colors)) == length(unique.tags))) {
+      warning("The length of colors vector must be equal to the number 
+            of different tags. As is it not the case, colors are ignored")
+    }
+    if (isTRUE(
+      sum(unique.tags %in% names(colors)) == length(unique.tags))) {
+      lapply(seq_len(length(colors)), function(x) {
+        list.tags <- which(names(colors)[x] == tags, arr.ind = TRUE)
+        openxlsx::addStyle(wb,
+          sheet = n,
+          cols = list.tags[, "col"],
+          rows = list.tags[, "row"] + 1,
+          style = openxlsx::createStyle(fgFill = colors[x])
+        )
+      })
+    }
+  }
+  return(wb)
+}
+
+
+
+#' @export
+#' 
+#' 
+Write_SamplesData_to_Excel <- function(wb, obj, n){
+  openxlsx::addWorksheet(wb, "Samples Meta Data")
+  openxlsx::writeData(wb, 
+    sheet = n, 
+    SummarizedExperiment::colData(obj), 
+    rowNames = FALSE)
+  
+  
+  # Add colors for sample data sheet
+  u_conds <- unique(omXplore::get_group(obj))
+  colors <- stats::setNames(ExtendPalette(length(u_conds)), u_conds)
+  colors[["blank"]] <- "white"
+  
+  tags <- as.data.frame(SummarizedExperiment::colData(obj))
+  tags[, ] <- "blank"
+  tags$Sample.name <- omXplore::get_group(obj)
+  tags$Condition <- omXplore::get_group(obj)
+  
+  unique.tags <- NULL
+  if (!is.null(tags) && !is.null(colors)) {
+    unique.tags <- unique(as.vector(as.matrix(tags)))
+    if (!isTRUE(
+      sum(unique.tags %in% names(colors)) == length(unique.tags))) {
+      warning("The length of colors vector must be equal to the number 
+            of different tags. As is it not the case, colors are ignored")
+    }
+    if (isTRUE(
+      sum(unique.tags %in% names(colors)) == length(unique.tags))) {
+      lapply(seq_len(length(colors)), function(x) {
+        list.tags <- which(names(colors)[x] == tags, arr.ind = TRUE)
+        openxlsx::addStyle(wb,
+          sheet = n,
+          cols = list.tags[, "col"],
+          rows = list.tags[, "row"] + 1,
+          style = openxlsx::createStyle(fgFill = colors[x])
+        )
+      })
+    }
+  }
+  
+  return(wb)
+}
+
+
+Write_Feature_Data <- function(obj, i, n){
+if (dim(Biobase::fData(obj))[2] != 0) {
+  openxlsx::addWorksheet(wb, "Feature Meta Data")
+  openxlsx::writeData(wb,
+    sheet = n,
+    cbind(
+      ID = rownames(Biobase::fData(obj)),
+      Biobase::fData(obj)
+    ), rowNames = FALSE
+  )
+}
+
+colors <- as.list(stats::setNames(mc$color, mc$node))
+tags <- cbind(
+  keyId = rep("Quant. by direct id", nrow(obj)),
+  Biobase::fData(obj)
+)
+
+.ind <- colnames(Biobase::fData(obj))
+.ind <- which(.ind %in% obj@experimentData@other$names_metacell)
+tags[, ] <- "Quant. by direct id"
+tags[, 1 + .ind] <- GetMetacell(obj)
+
+unique.tags <- NULL
+if (!is.null(tags) && !is.null(colors)) {
+  unique.tags <- unique(as.vector(as.matrix(tags)))
+  if (!isTRUE(
+    sum(unique.tags %in% names(colors)) == length(unique.tags))) {
+    warning("The length of colors vector must be equal to the number
+          of different tags. As is it not the case, colors are ignored")
+  }
+  if (isTRUE(
+    sum(unique.tags %in% names(colors)) == length(unique.tags))) {
+    lapply(seq_len(length(colors)), function(x) {
+      list.tags <- which(names(colors)[x] == tags, arr.ind = TRUE)
+      openxlsx::addStyle(wb,
+        sheet = n,
+        cols = list.tags[, "col"],
+        rows = list.tags[, "row"] + 1,
+        style = openxlsx::createStyle(fgFill = colors[x])
+      )
+    })
+  }
+
+}
+
+
+return(wb)
+}
+
+#'
+#' @title This function exports a \code{MSnSet} object to a Excel file.
+#' 
+#' @description 
+#' This function exports a \code{MSnSet} data object to a Excel file.
+#' Each of the three data.frames in the \code{MSnSet} object (ie experimental
+#' data, phenoData and metaData are respectively integrated into separate sheets
+#' in the Excel file). 
+#' 
+#' The colored cells in the experimental data correspond to the original
+#' missing values which have been imputed.
+#'
+#' @param obj An object of class \code{MSnSet}.
+#'
+#' @param filename A character string for the name of the Excel file.
+#'
+#' @return A Excel file (.xlsx)
+#'
+#' @author Samuel Wieczorek
+#'
+#' @examples
+#' data(Exp1_R25_pept, package="DaparToolshedData")
+#' obj <- Exp1_R25_pept[seq_len(10)]
+#' write_QF_To_Excel(obj, "foo.xlsx")
+#' 
+#'
+#' @export
+#'
+#'
+write.excel <- function(obj, filename) {
+  pkgs.require(c('stats', 'openxlsx'))
+  if (!inherits(obj, "QFeatures")){
+    message('Obj is not a QFeatures')
+  return(NULL)
+  }
+  name <- filename
+  wb <- openxlsx::createWorkbook(filename)
+  
+  
+  n <- 1
+  wb <- Write_SamplesData_to_Excel(wb, obj, n)
+ 
+  for (i in seq(length(obj))){
+    print(i)
+    print(n)
+    wb <- write_Assay_To_Excel(wb, obj, i, n = n + i)
+  }
+
+  ## Add feature Data sheet
+  
+   n <- n + length(obj) + 1
+  #wb <- Write_Feature_Data(obj, i, n)
+
+  
+  openxlsx::saveWorkbook(wb, filename, overwrite = TRUE)
+  return(filename)
+}
+
