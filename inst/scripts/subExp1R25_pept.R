7b6bcf26424acb54d58f38410e632422e849c9ce
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2026-02-16 14:47:40 +0100

    First commit

--- inst/scripts/subExp1R25_pept.R
+++ inst/scripts/subExp1R25_pept.R
@@ -10,9 +10,56 @@
 #' @export
 
 builds_sub_Exp1_R25_pept <- function(){
-  library(DaparToolshedData)
-  data(Exp1_R25_pept)
-  subR25pept <- Exp1_R25_pept[1:100]
+  
+  if (! requireNamespace("DaparToolshed", quietly = TRUE)) {
+    stop("Please install DaparToolshed: BiocManager::install('DaparToolshed')")
+  }
+  
+  exprsFile <- system.file(
+    "extdata/data", 
+    "Exp1_R25_pept_100.txt", 
+    package = "DaparToolshed"
+  )
+  
+  qdata = read.table(
+    exprsFile, 
+    header = TRUE, 
+    sep = "\t", 
+    as.is = TRUE
+  )
+  
+  metadataFile <- system.file(
+    "extdata/data", 
+    "samples_Exp1_R25.txt", 
+    package = "DaparToolshed"
+  )
+  
+  metadata = read.table(
+    metadataFile, 
+    header = TRUE, 
+    sep = "\t", 
+    as.is = TRUE
+  )
+  
+  
+  
+  subR25pept <- createQFeatures(
+    data = qdata,
+    file = 'subExp1R25_100_pept',
+    sample = metadata,
+    indQData = seq.int(from = 56, to = 61),
+    keyId = "Sequence",
+    indexForMetacell = seq.int(from = 43, to = 48),
+    logData = TRUE,
+    force.na = TRUE,
+    typeDataset = 'peptide',
+    parentProtId = "Protein_group_IDs",
+    analysis = "Pept_data",
+    description = NULL,
+    processes = NULL,
+    typePipeline = NULL,
+    name.pipeline = NULL,
+    software = 'maxquant') 
   
   save(subR25pept, file = 'data/subR25pept.RData', compress='xz')
 
--- inst/scripts/subExp1R25_prot.R
+++ inst/scripts/subExp1R25_prot.R
@@ -10,9 +10,56 @@
 #' @export
 
 builds_sub_Exp1_R25_prot <- function(){
-  library(DaparToolshedData)
-  data(Exp1_R25_prot)
-  subR25prot <- Exp1_R25_prot[1:100]
+  
+  if (! requireNamespace("DaparToolshed", quietly = TRUE)) {
+    stop("Please install DaparToolshed: BiocManager::install('DaparToolshed')")
+  }
+  
+  exprsFile <- system.file(
+    "extdata/data", 
+    "Exp1_R25_prot_100.txt", 
+    package = "DaparToolshed"
+  )
+  
+  qdata = read.table(
+    exprsFile, 
+    header = TRUE, 
+    sep = "\t", 
+    as.is = TRUE
+  )
+  
+  metadataFile <- system.file(
+    "extdata/data", 
+    "samples_Exp1_R25.txt", 
+    package = "DaparToolshed"
+  )
+  
+  metadata = read.table(
+    metadataFile, 
+    header = TRUE, 
+    sep = "\t", 
+    as.is = TRUE
+  )
+  
+  
+  
+  subR25prot <- createQFeatures(
+    data = qdata,
+    file = 'subExp1R25_100_prot',
+    sample = metadata,
+    indQData = seq.int(from = 49, to = 54),
+    keyId = "Majority_protein_IDs",
+    indexForMetacell = seq.int(from = 36, to = 41),
+    logData = TRUE,
+    force.na = TRUE,
+    typeDataset = 'protein',
+    parentProtId = NULL,
+    analysis = "Protein_data",
+    description = NULL,
+    processes = NULL,
+    typePipeline = NULL,
+    name.pipeline = NULL,
+    software = 'maxquant') 
   
   
   save(subR25prot, file = 'data/subR25prot.RData', compress='xz')
